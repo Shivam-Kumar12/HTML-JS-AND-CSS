d30a3c5f55d9b00170314904b3dbd7c991ff0a4a
Shivam Kumar <109784057+Shivam-Kumar12@users.noreply.github.com>
2023-02-07 15:42:32 +0530

    Add files via upload

---
+++ fin.java
@@ -0,0 +1,14 @@
+final class test
+{
+void fun()
+{
+System.out.println("Hello this function is base class");
+}
+}
+class test1 extends test
+{
+final void fun()
+{
+System.out.println("hello this is the another function");
+}
+}
