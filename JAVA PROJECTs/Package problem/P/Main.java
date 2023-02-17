0c863a69e3b572326af2b1ea7e2da85750497358
Shivam Kumar <109784057+Shivam-Kumar12@users.noreply.github.com>
2023-02-17 21:36:56 +0530

    Update Main.java

--- JAVA PROJECTs/Package problem/P/Main.java
+++ JAVA PROJECTs/Package problem/P/Main.java
@@ -1,8 +1,6 @@
-//2) Create a
-//class TwoDim which contains private members as x and y coordinates in package
+//2) Create a class TwoDim which contains private members as x and y coordinates in package
 //PI. Define the default constructor, a parameterized constructor and override toString() method to display the co-ordinates. Now reuse this class and in
-//package P2 create another class ThreeDim. adding a new dimension as z as its
-//private member. Define the constructors for the subclass and override toStringQ
+//package P2 create another class ThreeDim. adding a new dimension as z as its private member. Define the constructors for the subclass and override toStringQ
 //method in the subclass also. Write appropriate methods to show dynamic method
 //dispatch. The main() function should be in a package P.
 
