4ad9b504aa288c60654f80aad7f30abed6d8b356
Shivam Kumar <109784057+Shivam-Kumar12@users.noreply.github.com>
2023-02-17 21:33:29 +0530

    Add files via upload

---
+++ JAVA PROJECTs/Package problem/P/Main.java
@@ -0,0 +1,24 @@
+package P;
+
+// import java.util.Scanner;
+
+import P1.*;
+import P2.*;
+
+public class Main
+{
+    
+    public static void main(String args[])
+    {
+       TwoDim td=new TwoDim(1,5);
+        System.out.println(td);
+
+        td=new ThreeDim(3,4,6);
+        System.out.println(td);
+    
+
+}
+
+
+   
+}
