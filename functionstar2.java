b524d187a84df501226a3d822255eac108d9e18b
Shivam Kumar <109784057+Shivam-Kumar12@users.noreply.github.com>
2023-02-25 21:16:58 +0530

    Add files via upload

---
+++ functionstar2.java
@@ -0,0 +1,22 @@
+public class star{
+public static void main(String[]arg){
+int i=1;
+int j=1;
+int x;
+
+star s=new star();
+x=s.p1();
+}
+int p1()
+{
+for (int  i= 1; i <= 5; i++)
+{
+  for(int  j=5; j>=i ;j--)
+{
+System.out.print(" *");
+ }
+  System.out.println("");
+}
+ return 0;
+}
+ }
